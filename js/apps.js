5f488025128201904a0ab77dd2acc17cda37c320
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-27 16:42:10 +0600

    show country ui

--- js/apps.js
+++ js/apps.js
@@ -1,7 +1,23 @@
 const loadCountries = () =>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayData(data))
+}
+
+const displayData = (data) =>{
+    const allCountry = document.getElementById('all-country')
+    data.forEach((country) =>{
+        console.log(country)
+        const ceratDiv = document.createElement('div');
+        ceratDiv.classList.add('country')
+
+        ceratDiv.innerHTML = `
+        <img src="${country.flags.png}" alt="">
+        <h1>Name :${country.name.common} </h1> 
+        <p>Capital : ${country.capital ? country.capital[0] : 'No Capital'}</p>
+        `;
+        allCountry.appendChild(ceratDiv)
+    })
 }
 
 loadCountries()
