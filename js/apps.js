46107be322808062bb5a17f69db820b121839b29
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-27 17:04:19 +0600

    show country

--- js/apps.js
+++ js/apps.js
@@ -7,17 +7,26 @@ const loadCountries = () =>{
 const displayData = (data) =>{
     const allCountry = document.getElementById('all-country')
     data.forEach((country) =>{
-        console.log(country)
         const ceratDiv = document.createElement('div');
         ceratDiv.classList.add('country')
 
         ceratDiv.innerHTML = `
-        <img src="${country.flags.png}" alt="">
+        <img class="img" src="${country.flags.png}" alt="">
         <h1>Name :${country.name.common} </h1> 
         <p>Capital : ${country.capital ? country.capital[0] : 'No Capital'}</p>
+       <button onclick="loadCountriesDetails('${country.cca2}')">Details</button>
         `;
         allCountry.appendChild(ceratDiv)
     })
 }
+const loadCountriesDetails =(code) =>{
+   const URL = `https://restcountries.com/v3.1/alpha/${code}`;
+   fetch(URL)
+   .then(res => res.json())
+    .then(data => disLoadplayData(data[0]))
+}
 
+const disLoadplayData =(country)=>{
+    
+}
 loadCountries()
